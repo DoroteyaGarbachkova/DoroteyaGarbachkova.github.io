7d85fef6e8302b8c392446dd4d33634ca9496228
DoroteyaGarbachkova <doroteya.garbachkova@gmail.com>
2016-06-13 17:42:59 +0300

    js

---
+++ javascript/tasks/1/task1.js
@@ -0,0 +1,22 @@
+var a=5;
+var b=6;
+console.log(a+b);
+console.log(typeof a);
+
+var greater=(a===b);
+console.log(greater);
+console.log(typeof greater);
+
+var lname='Ivanov';
+console.log('Ivan'+' '+lname);
+console.log(typeof lname);
+
+var y=undefined;
+console.log(typeof y);
+
+var x=null;
+console.log(typeof x);
+
+var z;
+console.log(typeof z);
+
