7d20a39ec274e23d42b9e681ba042fab9d5ab2c6
DoroteyaGarbachkova <doroteya.garbachkova@gmail.com>
2016-06-14 10:11:39 +0300

    task1

--- javascript/tasks/1/task1.js
+++ javascript/tasks/1/task1.js
@@ -1,22 +1,47 @@
-var a=5;
-var b=6;
-console.log(a+b);
+//numbers
+var a = 5;
 console.log(typeof a);
+var b = 6.5;
+var ab=a + b;
+console.log(ab);
+console.log(typeof ab);
 
-var greater=(a===b);
+//boolean
+var greater = (a === b);
 console.log(greater);
 console.log(typeof greater);
+var c = true;
+console.log(typeof true);
 
-var lname='Ivanov';
-console.log('Ivan'+' '+lname);
+//string
+var lname = 'Ivanov';
+console.log('Ivan' + ' ' + lname);
 console.log(typeof lname);
+var name = 'Mitko' + ' ' + 'Kirilov';
+console.log(typeof name);
+var quotedText = 'How you doin' + ' ?' + ', Joey said';
+console.log(quotedText);
+console.log(typeof quotedText);
+var str = '';
+console.log(typeof str);
 
-var y=undefined;
+
+//undefined
+var y = undefined;
 console.log(typeof y);
-
-var x=null;
-console.log(typeof x);
-
 var z;
 console.log(typeof z);
 
+//object
+var x = null;
+console.log(typeof x);
+var arr = [];
+console.log(typeof arr);
+var obj = {};
+console.log(typeof {});
+var abc = 3;
+abc = null;
+console.log(typeof abc);
+var d = 9;
+var d = new Number(2);
+console.log(typeof d);
