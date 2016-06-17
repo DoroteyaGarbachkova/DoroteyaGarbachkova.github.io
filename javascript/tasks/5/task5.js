a04f5dcf6e5b46a3c723379c5e90a09a0fa29e9b
DoroteyaGarbachkova <doroteya.garbachkova@gmail.com>
2016-06-17 16:39:34 +0300

    task5 finish

--- javascript/tasks/5/task5.js
+++ javascript/tasks/5/task5.js
@@ -27,31 +27,50 @@ console.log(comp3);
 ////////////////////////////////////////////////////////////////////////////
 console.log('Problem 3');
 
-function maximalSequence(arr) {
-	//arr=[2, 1, 1, 2, 3, 3, 2, 2, 2, 1];
-    var currentSequence = 1,
-        longestSequence = arr[0],
-        mostFrequentNum = arr[0];
 
-    for (var i = 1; i < arr.length; i++) {
-        if (arr[i - 1] === arr[i]) {
-            currentSequence++;
-            if (longestSequence >= currentSequence) {
-                longestSequence = currentSequence;
-                mostFrequentNum = arr[i];
-            }
-        } else {
-            currentSequence = 1;
+var randomElements = [2, 1, 1, 2, 3, 3, 2, 2, 2,2, 1];
+var currentSequence = 1,
+    longestSequence = 1,
+    indexOfLongest = 0;
+
+for (var i = 0; i < randomElements.length - 1; i++) {
+    if (randomElements[i] === randomElements[i + 1]) {
+        currentSequence++;
+
+        if (longestSequence <= currentSequence) {
+            longestSequence = currentSequence;
+            indexOfLongest = randomElements[i];
+            //console.log(i);
         }
+    } else {
+        currentSequence = 1;
+
     }
-    var result = [arr[0]];
-    for (var i = 0; i <longestSequence; i++) {
-        result.push(mostFrequentNum);
-        //console.log(result.join(', '));
-    }
-    console.log(result.join(', '));
 }
-maximalSequence([2, 1, 1, 2, 3, 3, 2, 2, 2, 1]);
+
+console.log(longestSequence + "times- " + indexOfLongest);
+
+////////////////////////////////////////////////////////////////////////////
+console.log('Problem 4');
+var randomElements = [3, 2, 3, 4, 2, 2, 4];
+var currentSequence = 1;
+var longestSequence = 1;
+var indexOfLongest;
+for (var i = 0; i < randomElements.length - 1; i++) {
+    if (randomElements[i] < randomElements[i + 1]) {
+        currentSequence++;
+    } else {
+        if (longestSequence < currentSequence) {
+            longestSequence = currentSequence;
+            indexOfLongest = i - currentSequence + 1;
+            //console.log(i);
+        }
+        currentSequence = 1;
+    }
+}
+var result = randomElements.slice(indexOfLongest, indexOfLongest + longestSequence);
+console.log(result);
+
 ////////////////////////////////////////////////////////////////////////////
 console.log('Problem 5');
 
@@ -68,23 +87,51 @@ var currentSequence = 1,
     longestSequence = 1,
     mostFrequentNum = arr[0];
 
+
 for (var i = 0; i < arr.length - 1; i++) {
     if (arr[i] == arr[i + 1]) {
         currentSequence++;
         if (longestSequence <= currentSequence) {
             longestSequence = currentSequence;
             mostFrequentNum = arr[i];
+
+        } else {
+            currentSequence = 1;
         }
-    } else {
-        currentSequence = 1;
+
     }
 }
 console.log("Most frequent number: " + mostFrequentNum);
+
+
 ///////////////////////////////////////////
 console.log('Problem 7');
 
-var arr = [5, 9, 30, 1, 3, 23, 7, 8,21,78,0];
+var arr = [5, 9, 30, 1, 3, 23, 7, 8, 21, 78, 0];
 var sort = arr.sort(function(a, b) {
+    var end = sort.length - 1;
     return a - b
 });
 console.log(sort);
+var x = 30;
+var start = 0;
+var endd = arr.length - 1;
+var mid;
+
+while (start <= endd && arr[mid] != x) {
+    mid = parseInt((start + endd) / 2);
+
+    if (x < arr[mid]) {
+        endd = mid - 1;
+
+    }
+    if (x > arr[mid]) {
+        start = mid + 1;
+
+    }
+
+
+}
+
+console.log("Index:" + mid);
+//////////////////////////////////
