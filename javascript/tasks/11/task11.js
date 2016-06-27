c7e74c39d5f4ddd925b8be6de96c5ff41051dfe2
DoroteyaGarbachkova <doroteya.garbachkova@gmail.com>
2016-06-27 17:14:04 +0300

    task11 throws

--- javascript/tasks/11/task11.js
+++ javascript/tasks/11/task11.js
@@ -1,14 +1,46 @@
-var ul=document.getElementById('list');
+var ul = document.getElementById('list');
 ul.parentNode.removeChild(ul);
-function lists(){
-var vegetables=['tomato','potato','carrot','cabbage','cucumber'];
-var list = '<ul>',
-    count=5,
-    i;
-for (var i = 0; i < count; i++) {
-    list += '<li>' + vegetables[i] + '</li>';
+var vegetables = ['tomato', 'potato', 'carrot', 'cabbage', 'cucumber'];
+
+function checkedArr() {
+    if (Array.isArray(vegetables)) {
+        console.log("Is array");
+    } else {
+        console.log("Is not array");
+    }
 }
-list += '</ul>';
-document.body.innerHTML+=list;
+
+function checkedArrEl() {
+    var i;
+    for ( i = 0; i < vegetables.length; i++) {
+
+        if (typeof(vegetables[i]) !== 'string'  && typeof(vegetables[i]) !== 'number') {
+           return console.log("Error");
+        } 
+    }
+    return console.log("Is not error");
+}
+
+function chekedId() {
+    if (ul == undefined) {
+        console.log("Error");
+    } else {
+        console.log("Exist");
+    }
+}
+
+function lists() {
+
+    var list = '<ul>',
+        count = 5,
+        i;
+    for (var i = 0; i < count; i++) {
+        list += '<li>' + vegetables[i] + '</li>';
+    }
+    list += '</ul>';
+    document.body.innerHTML += list;
 }
 lists()
+checkedArr()
+checkedArrEl()
+chekedId()
